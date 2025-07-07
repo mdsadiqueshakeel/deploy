75681e4f521aa7bc85a5ba144ba018f32ec3230c
mdsadiqueshakeel <mdsadiqueshakeel@gmail.com>
2025-07-07 18:19:31 +0530

    Product added

--- backend/wallet-service/src/Constraints/ProductList.js
+++ backend/wallet-service/src/Constraints/ProductList.js
@@ -104,6 +104,27 @@ const PRODUCT_LIST = [
     mrp: 500,
     image: '/assets/images/i6.jpg',
   },
+  {
+    productCode: 1050,
+    name: 'Softcare Sanitary Pad (30 Pcs) ',
+    dp: 525,
+    mrp: 900,
+    image: '/assets/images/i7.jpg',
+  },
+  {
+    productCode: 1055,
+    name: 'Alkaline water bottle',
+    dp: 3199,
+    mrp: 5199,
+    image: '/assets/images/i8.jpg',
+  },
+  {
+    productCode: 1065,
+    name: 'Combo of 7',
+    dp: 5199,
+    mrp: 8599,
+    image: '/assets/images/i9.jpg',
+  },
 ];
 
 module.exports = PRODUCT_LIST;
